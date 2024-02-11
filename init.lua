48c80c07bd540627c9d6a8399fbb28e11b2e9215
Dennis <dennis.dair@gmail.com>
2024-02-10 23:28:17 -0500

    add tailwind colorizer

--- init.lua
+++ init.lua
@@ -51,6 +51,16 @@ require('packer').startup(function(use)
     after = 'nvim-treesitter',
   }
 
+  use({
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    -- optionally, override the default options:
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+    end
+  })
+
   use { 'mhartington/formatter.nvim' }
 
   use { 'folke/todo-comments.nvim' }
