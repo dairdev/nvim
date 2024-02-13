bc9b856bc0c567482a1cdab59adef75d22df1dd2
Dennis <dennis.dair@gmail.com>
2024-02-12 23:14:39 -0500

    fix nvim-surround setup

--- init.lua
+++ init.lua
@@ -115,9 +115,9 @@ require('packer').startup(function(use)
     "kylechui/nvim-surround",
     tag = "*", -- Use for stability; omit to use `main` branch for the latest features
     config = function()
-      require("nvim-surround").setup({
+      require("nvim-surround").setup{
         -- Configuration here, or leave empty to use defaults
-      })
+      }
     end
   })
 
