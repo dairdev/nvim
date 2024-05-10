771296f20c8f1602a63ea933f85c2792e204f37a
Dennis <dennis.dair@gmail.com>
2024-05-09 21:07:36 -0500

    add right LuaSnip install packer entry

--- init.lua
+++ init.lua
@@ -34,6 +34,14 @@ require('packer').startup(function(use)
     },
   }
 
+  use({
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!:).
+    run = "make install_jsregexp"
+  })
+
   use { -- Autocompletion
     'hrsh7th/nvim-cmp',
     requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
