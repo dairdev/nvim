f3d5ef687226401dc4467e0392f214410895870a
Dennis <dennis.dair@gmail.com>
2024-06-08 18:00:47 -0500

    add rest-nvim

--- init.lua
+++ init.lua
@@ -86,7 +86,7 @@ require('packer').startup(function(use)
 
   use { 'folke/todo-comments.nvim' }
 
-  use 'simrat39/symbols-outline.nvim'
+  -- use 'simrat39/symbols-outline.nvim'
 
   use 'windwp/nvim-ts-autotag'
 
@@ -136,9 +136,15 @@ require('packer').startup(function(use)
     end
   })
 
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use 'MunifTanjim/prettier.nvim'
   use 'brenoprata10/nvim-highlight-colors'
+
+  use {
+    "rest-nvim/rest.nvim",
+    rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+    config = function()
+      require("rest-nvim").setup()
+    end,
+  }
 
   use {
   "loctvl842/monokai-pro.nvim",
@@ -534,62 +540,6 @@ cmp.config.formatting = {
   format = tailwindcss
 }
 
-local null_ls = require("null-ls")
-
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
-
-null_ls.setup({
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-
-      -- format on save
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-      vim.api.nvim_create_autocmd(event, {
-        buffer = bufnr,
-        group = group,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
-        end,
-        desc = "[lsp] format on save",
-      })
-    end
-
-    if client.supports_method("textDocument/rangeFormatting") then
-      vim.keymap.set("x", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-    end
-  end,
-  sources = {
-    null_ls.builtins.formatting.prettierd
-  }
-})
-
-local prettier = require("prettier")
-
-prettier.setup({
-  bin = 'prettierd',
-  filetypes = {
-    "css",
-    "graphql",
-    "html",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "less",
-    "markdown",
-    "scss",
-    "typescript",
-    "typescriptreact",
-    "yaml",
-  },
-})
-
 require("flutter-tools").setup {}
 
 require('nvim-highlight-colors').setup {}
@@ -702,7 +652,7 @@ require('lualine').setup {
   }
 }
 
-require("symbols-outline").setup()
+-- require("symbols-outline").setup()
 
 require'todo-comments'.setup()
 
